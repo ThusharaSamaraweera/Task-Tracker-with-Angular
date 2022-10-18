4bb74e15e7377dff892c3375515395e0641380d8
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-10-18 21:37:00 +0530

    create get tasks service with observable

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from 'src/app/Task.model';
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-task-item',
@@ -8,7 +9,8 @@ import { Task } from 'src/app/Task.model';
 })
 export class TaskItemComponent implements OnInit {
   @Input() task!: Task;
-  
+  faTimes: any = faTimes;
+
   constructor() { }
 
   ngOnInit(): void {
